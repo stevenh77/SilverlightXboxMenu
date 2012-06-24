ec31288cd24c3aa9a16b463bddfe0291a1e99c3a
Steven Hollidge <stevenhollidge@hotmail.com>
2012-06-24 14:52:53 +0100

    Menu header radio button implemented

---
+++ SilverlightXboxMenu/Controls/MenuHeaderRadioButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SilverlightXboxMenu.Controls
+{
+    public class MenuHeaderRadioButton : RadioButton
+    {
+        public static readonly DependencyProperty DisplayTextProperty =
+            DependencyProperty.Register("DisplayText", typeof(string), typeof(MenuHeaderRadioButton), new PropertyMetadata(default(string)));
+
+        public string DisplayText
+        {
+            get { return (string)GetValue(DisplayTextProperty); }
+            set { SetValue(DisplayTextProperty, value); }
+        }
+    }
+}
--- SilverlightXboxMenu/MainPage.xaml.cs
+++ SilverlightXboxMenu/MainPage.xaml.cs
@@ -1,20 +1,9 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-
-namespace SilverlightXboxMenu
+﻿namespace SilverlightXboxMenu
 {
-	public partial class MainPage : UserControl
+	public partial class MainPage
 	{
 		public MainPage()
 		{
-			// Required to initialize variables
 			InitializeComponent();
 		}
 	}
